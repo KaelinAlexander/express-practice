6936c01fe27fe4b21d096660879d7286141ad9a8
KaelinAlexander <kaelin.alexander@gmail.com>
2021-01-06 11:17:26 -0700

    adds basic JSON requests and middleware to log requests

---
+++ Members.js
@@ -0,0 +1,29 @@
+const members = [
+    {
+        id: 1,
+        name: 'John',
+        email: 'john@john.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Jean',
+        email: 'jean@john.com',
+        status: 'active'
+    },
+    {
+        id: 3,
+        name: 'Jonn',
+        email: 'jonn@john.com',
+        status: 'active'
+    },
+    {
+        id: 4,
+        name: 'Sean',
+        email: 'sean@john.com',
+        status: 'active'
+    },
+];
+
+module.exports = members;
+
--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
+const moment = require('moment');
+const members = require('./Members');
+
+const PORT = process.env.PORT || 3000;
+
+const logger = (req, res, next) => {
+   console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}: ${moment().format()}`);
+   next(); 
+}
+
+app.listen((PORT), () => console.log(`Connection established on port ${PORT}`));
+
+// Init Middleware
+
+app.use(logger);
 
 // Set static folder:
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = process.env.PORT || 3000;
+// Set other routes:
 
-app.listen((PORT), () => console.log(`Connection established on port ${PORT}`));
+// Gets all members:
+app.get('/api/members', (req, res) => {
+    res.json(members);
+});
+
+//
 
