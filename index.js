892c08b01e45caee891e90f12aa4234507bc0249
KaelinAlexander <kaelin.alexander@gmail.com>
2021-01-06 11:00:13 -0700

    adds initial routing and renders for static pages

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Set static folder:
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen((PORT), () => console.log(`Connection established on port ${PORT}`));
+
