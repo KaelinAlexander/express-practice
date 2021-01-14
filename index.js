323631766cf60662142d474f726e071b4a7fa555
KaelinAlexander <kaelin.alexander@gmail.com>
2021-01-14 15:16:55 -0700

    sets up initial rendering engine files and dependencies

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const logger = require('./middleware/logger');
+const exphbs = require('express-handlebars')
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 
 app.listen((PORT), () => console.log(`Connection established on port ${PORT}`));
@@ -11,12 +12,21 @@ app.listen((PORT), () => console.log(`Connection established on port ${PORT}`));
 // Init Middleware
 app.use(logger);
 
+// Handlebars Middleware
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 // Body Parser Init
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
-// Set static folder:
-app.use(express.static(path.join(__dirname, 'public')));
+// Homepage Route
+
+app.get('/', (req, res) => res.render('index'));
+
+// // Set static folder:
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // Members API Routes:
 app.use('/api/members', require('./routes/api/members'))
