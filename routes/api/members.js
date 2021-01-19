d15eab64b7d078dbeadd7d20bd0f9f8fccf2f2f5
KaelinAlexander <kaelin.alexander@gmail.com>
2021-01-18 18:31:20 -0700

    Fixes for clarity.

--- routes/api/members.js
+++ routes/api/members.js
@@ -15,7 +15,7 @@ router.get('/:id', (req, res) => {
     if (found) {
     res.json(members.filter(member => member.id === parseInt(req.params.id) ))
     } else {
-        res.status(400).json({msg: `No member found with the id of ${req.params.id}.`})
+        res.status(400).json({msg: `No member found with the id of ${req.params.id}. Please try another.`})
     }
 })
 
